85f94b3803607daf46dc51d31818f13a44245c8d
Apolbaq <emilianopolanco5@gmail.com>
2021-10-14 23:38:08 +0200

    MOre circles

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ context.fillStyle = 'green';
 //context.fillRect(100 ,100, 400, 400);
 
 //center of contorl with a lot of parameters
-let xspace = 3;
-let yspace = 3;
-let radius = 6;
+let xspace = 0;
+let yspace = 5;
+let radius = 35;
 let random = false;
 let divisor = parseInt(Math.random()*10);
 console.log(divisor);
 let squareSide = 30;
-let dividendo = 3;
-context.lineWidth = 2;
+let dividendo = 2;
+context.lineWidth = 1;
 if(!random){
     divisor = dividendo;
 }
@@ -23,7 +23,7 @@ let condiStatement = 0;
 
 
 
-let yHolder = 100+radius;
+let yHolder = radius;
 
 for(let j = 0; j <= squareSide; j++){
     for (let i = 0; i < squareSide; i++) {
@@ -32,7 +32,7 @@ for(let j = 0; j <= squareSide; j++){
         context.stroke();
         if((i+j)%divisor==0) {
             context.beginPath();
-            context.arc(radiusxAnalicer(radius, i, xspace), yHolder, radius - 5, 0, Math.PI * 2);
+            context.arc(radiusxAnalicer(radius, i, xspace), yHolder, radius - 30, 0, Math.PI * 2);
             context.stroke();
             //Ho ho ho no one will know
         }
@@ -41,8 +41,8 @@ for(let j = 0; j <= squareSide; j++){
 }
 
 function radiusxAnalicer(radiuss, i,xxspace){
-    return 100+radiuss+i*2*(radiuss+xxspace) ;
+    return radiuss+i*2*(radiuss+xxspace) ;
 }
 function radiusyAnalicer(radiuss, j, yyspace){
-    return 100+radiuss+j*yyspace;
+    return radiuss+j*yyspace;
 }
